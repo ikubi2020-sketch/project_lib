656ef97c5e5f78b9310315a7aca3e322c4d038fd
yehonatan koobi <ikubi2020@gmail.com>
2026-06-14 11:02:36 +0300

    added func create_table_members and motifyed func create_table_books

--- database/db_connection.py
+++ database/db_connection.py
@@ -1,5 +1,5 @@
 import mysql.connector
-from logs.logging import logger
+from logs.logger_create import logger
 
 
 def get_connection():
@@ -16,20 +16,21 @@ def get_connection():
 
 
 def create_table_books():
-    logger.info("active func | create_table |")
+    logger.error("active func | create_table |")
     cursor = None
     try:
         connection = get_connection()
         cursor = connection.cursor()
         cursor.execute(
             """
-                create table if not exists books,
+                create table if not exists books(
                 id int primary key auto_increment,
                 title varchar(50) not null,
                 genre varchar(20) not null,
-                is_available boolean not null,
-                borrowed_by_member_id default null;
+                is_available boolean not null default True,
+                borrowed_by_member_id int default null);
             """)
+        logger.info("table books created")
     except Exception as e:
         logger.error(f"reach error {e}")
         return {"message" : f"reach error {e}"}
@@ -41,5 +42,53 @@ def create_table_books():
 
 
 
+def create_table_members():
+    logger.info("active func | create_table_members |")
+    cursor = None
+    try:
+        connection = get_connection()
+        cursor = connection.cursor()
+        cursor.execute("""
+                       create table if not exists members(
+                       id int primary key auto_increment,
+                       name varchar(50) not null,
+                       email varchar(100) unique not null,
+                       is_active boolean  not null default True,
+                       total_borrows int default 0);
+                       """)
+        logger.info("table members created")
+    except Exception as e:
+        logger.error(f"reach error {e}")
+        return {"message" : f"reach error {e}"}
+    finally:
+        if cursor is not None:
+            cursor.close()
+
+        
+
+
+
+
+
+
+
+
+
+
+def ______():
+    logger.info("active func |  |")
+    cursor = None
+    try:
+        connection = get_connection()
+        cursor = connection.cursor()
+        cursor.execute(";")
+    except Exception as e:
+        logger.error(f"reach error {e}")
+        return {"message" : f"reach error {e}"}
+    finally:
+        if cursor is not None:
+            cursor.close()
+
+        
 
 
