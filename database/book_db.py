74b86158aef8b37e19fa6e2c6f7627225f2b87d2
yehonatan koobi <ikubi2020@gmail.com>
2026-06-15 16:56:56 +0300

    added  most funcs in books_db

--- database/book_db.py
+++ database/book_db.py
@@ -7,9 +7,8 @@ from fastapi import HTTPException
 class booksDB:
     def create_book(self, data: dict):
         logger.info("active func | create_book |")
-        query = "insert into books (title, author, genre, is_available) values( %s, %s, %s, %s)"
-        params =  (data["title"], data["author"], data["genre"], data["is_available"])
-        cursor = None
+        query = "insert into books (title, author, genre, is_available) values( %s, %s, %s)"
+        params =  (data["title"], data["author"], data["genre"])
         try:
             run_query_dml(query, params)
             logger.info("book created")
@@ -17,15 +16,10 @@ class booksDB:
         except Exception as e:
             logger.error(f"reach error {e}")
             raise HTTPException(status_code=400, detail={"message" : f"reach error {e}"})
-        finally :
-            if cursor is not None:
-                cursor.close()
-
-
+        
 
     def get_all_books(self):
         logger.info("active func | get_all_books |")
-        cursor = None
         query = "select title from books"
         try:
             books_list = run_query_fetchall(query)
@@ -34,14 +28,10 @@ class booksDB:
         except Exception as e:
             logger.error(f"reach error {e}")
             return {"message" : f"reach error {e}"}
-        finally:
-            if cursor is not None:
-                cursor.close()
-
+        
 
     def get_book_by_id(self, id):
         logger.info("active func | get_book_by_id |")
-        cursor = None
         param = (id,)
         query = "select * from books where id = %s"
         try:
@@ -51,44 +41,91 @@ class booksDB:
         except Exception as e:
             logger.error(f"reach error {e}")
             raise HTTPException (status_code=400, detail={"message" : f"reach error {e}"})
-        finally:
-            if cursor is not None:
-                cursor.close()
-
-
+        
     def update_book(self, id, data):
-        pass 
-    
+        logger.info("active func | update_book |")
+        param = (data["title"], data["author"], data["genre"], id) 
+        query = "update  books  set title = %s, author = %s, genre = %s where id = %s;" 
+        try:
+            run_query_dml(query , param)
+            logger.info("update book")
+            return "updated book"
+        except Exception as e:
+            logger.error(f"reach error {e}")
+            raise HTTPException (status_code=400, detail={"message" : f"reach error {e}"})
+        
+        
     def set_available(self, id, val, member_id):
-        pass
+        logger.info("active func | set_available |")
+        query = ""
+        try:
 
+            logger.info("")
+        except Exception as e:
+            logger.error(f"reach error {e}")
+            raise HTTPException (status_code=400, detail={"message" : f"reach error {e}"})
+        
     def count_total_books(self):
-        pass
+        logger.info("active func | count_total_books |")
+        query = "select count(title) as totally from books;"
+        try:
+            count_books = run_query_fetchone(query)
+            logger.info("count of books sent out")
+            return count_books
+        except Exception as e:
+            logger.error(f"reach error {e}")
+            raise HTTPException (status_code=400, detail={"message" : f"reach error {e}"})
+        
 
     def count_available_books(self):
-        pass
+        logger.info("active func | count_available_books |")
+        query = "select count(*) as available from books  where is_available is True;"
+        try:
+            available = run_query_fetchone(query)
+            logger.info("count of available books sent out")
+            return available
+        except Exception as e:
+            logger.error(f"reach error {e}")
+            raise HTTPException (status_code=400, detail={"message" : f"reach error {e}"})
+        
 
     def count_borrowed_books(self):
-        pass
+        logger.info("active func | count_borrowed_books |")
+        query = "select count(*) as unavailable from books  where is_available is False;"
+        try:
+            unavailable = run_query_fetchone(query)
+            logger.info("sent borrowed books")
+            return unavailable
+        except Exception as e:
+            logger.error(f"reach error {e}")
+            raise HTTPException (status_code=400, detail={"message" : f"reach error {e}"})
+        
 
-    def count_by_genre(self, genre):
-        pass
+    def count_by_genre(self, genre:str):
+        logger.info("active func | count_by_genre |")
+        param = (genre,)
+        query = "select count(*) as my_genre from books where genre = %s;"
+        try:
+            genre_count = run_query_fetchone(query, param)
+            logger.info("return count by genre")
+            return genre_count
+        except Exception as e:
+            logger.error(f"reach error {e}")
+            raise HTTPException (status_code=400, detail={"message" : f"reach error {e}"})
+        
+
     
     def count_active_borrows_by_member(self, member_id):
-        pass
+        logger.info("active func | count_active_borrows_by_member |")
+        param = (member_id,)
+        query = "select total_borrows from members where id = %s;"
+        try:
+            borrows_num = run_query_fetchone(query, param)
+            logger.info("borrow number sent out")
+            return borrows_num
+        except Exception as e:
+            logger.error(f"reach error {e}")
+            raise HTTPException (status_code=400, detail={"message" : f"reach error {e}"})
 
 
 
-
-def ______():
-    logger.info("active func |  |")
-    cursor = None
-    try:
-        logger.info("")
-
-    except Exception as e:
-        logger.error(f"reach error {e}")
-        raise HTTPException (status_code=400, detail={"message" : f"reach error {e}"})
-    finally:
-        if cursor is not None:
-            cursor.close()
